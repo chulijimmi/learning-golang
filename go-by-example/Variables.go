4273c4b66d03b06adb10dd62c61367cbba51fcd3
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2022-08-04 19:01:27 +0800

    feature: add Variables and Constants

---
+++ go-by-example/Variables.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	//var declare a as initial
+	var a = "initial"
+	fmt.Println(a)
+
+	//var declare b,c as int
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	//var declare d as boolean
+	var d = true
+	fmt.Println(d)
+
+	//var declare e as integer
+	var e int
+	fmt.Println(e)
+
+	//shorthand var declaration :=
+	f := "apple"
+	fmt.Println(f)
+
+	//Print data type
+	fmt.Printf("a-type: %s\n", reflect.TypeOf(a))
+	fmt.Printf("b-type: %s\n", reflect.TypeOf(a))
+	fmt.Printf("d-type: %s\n", reflect.TypeOf(d))
+	fmt.Printf("e-type: %s\n", reflect.TypeOf(e))
+}
