28eff56e542ab125c6c71585d8b25bfbce4827c0
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2022-11-11 23:56:29 +0800

    Feature: add statements

---
+++ language_specification/Statements.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+
+func terminatingStatements() {
+	fmt.Println("============ Terminating Statements ============")
+}
+
+func emptyStatements() {
+	fmt.Println("============ Empty Statements ============")
+}
+
+func labeledStatements() {
+	fmt.Println("============ Labeled Statements ============")
+}
+
+func expressionStatements() {
+	fmt.Println("============ Expression Statements ============")
+}
+
+func sendStatements() {
+	fmt.Println("============ Send Statements ============")
+}
+
+func incdecStatements() {
+	fmt.Println("============ IncDec Statements ============")
+}
+
+func assignmentStatements() {
+	fmt.Println("============ Assignment Statements ============")
+}
+
+func ifStatements() {
+	fmt.Println("============ If Statements ============")
+}
+
+func switchStatements() {
+	fmt.Println("============ Switch Statements ============")
+}
+
+func forStatements() {
+	fmt.Println("============ For Statements ============")
+}
+
+func goStatements() {
+	fmt.Println("============ Go Statements ============")
+}
+
+func selectStatements() {
+	fmt.Println("============ Select Statements ============")
+}
+
+func returnStatements() {
+	fmt.Println("============ Return Statements ============")
+}
+
+func breakStatements() {
+	fmt.Println("============ Break Statements ============")
+}
+
+func continueStatements() {
+	fmt.Println("============ Continue Statements ============")
+}
+
+func gotoStatements() {
+	fmt.Println("============ Goto Statements ============")
+}
+
+func falthroughStatements() {
+	fmt.Println("============ Falthrough Statements ============")
+}
+
+func deferStatements() {
+	fmt.Println("============ Defer Statements ============")
+}
+
+func main() {
+	fmt.Println("============ Statements ============")
+	terminatingStatements()
+	emptyStatements()
+	labeledStatements()
+	expressionStatements()
+	sendStatements()
+	incdecStatements()
+	assignmentStatements()
+	ifStatements()
+	switchStatements()
+	forStatements()
+	goStatements()
+	selectStatements()
+	returnStatements()
+	breakStatements()
+	continueStatements()
+	gotoStatements()
+	falthroughStatements()
+	deferStatements()
+}
