0c470aab3a43730d67e0b0c8fbc9bf28e8db761a
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2022-11-11 23:47:22 +0800

    Add expression

--- language_specification/Expression.go
+++ language_specification/Expression.go
@@ -154,6 +154,160 @@ func variadic() {
 
 }
 
+func instantiations() {
+	min := func(x, y int) int {
+		return x + y
+	}
+	fmt.Println(min(2, 3))
+}
+
+func typeUnification() {
+	type a []map[int]bool
+	b := make(a, 2)
+	fmt.Printf("b: %v\n", b)
+}
+
+func operators() {
+	// Expression = UnaryExpr | Expression binary_op Expression .
+	// UnaryExpr  = PrimaryExpr | unary_op UnaryExpr .
+
+	// binary_op  = "||" | "&&" | rel_op | add_op | mul_op .
+	// rel_op     = "==" | "!=" | "<" | "<=" | ">" | ">=" .
+	// add_op     = "+" | "-" | "|" | "^" .
+	// mul_op     = "*" | "/" | "%" | "<<" | ">>" | "&" | "&^" .
+
+	// unary_op   = "+" | "-" | "!" | "^" | "*" | "&" | "<-" .
+
+	var a [8]byte
+	fmt.Printf("a: %v\n", a)
+	var s uint = 33
+	fmt.Printf("s: %v\n", s)
+
+	var s1 = 1 << s
+	fmt.Printf("i: %v\n", s1)
+	var s2 int32 = 1 << 2
+	fmt.Printf("s2: %v\n", s2)
+	var s3 = uint64(1 << s)
+	fmt.Printf("s3: %v\n", s3)
+	var s4 int = 1.0 << s
+	fmt.Printf("s4: %v\n", s4)
+	var s5 = 1.0<<s == s2
+	fmt.Printf("s5: %v\n", s5)
+
+	// Operato predecence
+	var n1 = 5
+	var n2 = 10
+	if n1 < 5 || n2 > n1 {
+		fmt.Println("result predecence 1")
+	}
+
+	// floating point operator
+	r := 0
+	x := 1
+	y := 2
+	z := 3
+
+	r = x*y + z
+	fmt.Printf("r: %v\n", r)
+	r = z
+	fmt.Printf("r: %v\n", r)
+	r += x * y
+	fmt.Printf("r: %v\n", r)
+
+	// string concatenation
+	stra := "kitty"
+	strb := "hi " + string(stra)
+	strb += " and good bye"
+	fmt.Printf("strb: %v\n", strb)
+}
+
+func comparisonOperator() {
+	// ==    equal
+	// !=    not equal
+	// <     less
+	// <=    less or equal
+	// >     greater
+	// >=    greater or equal
+	eq := 1 == 1
+	fmt.Printf("eq: %v\n", eq)
+	neq := !true
+	fmt.Printf("neq: %v\n", neq)
+}
+
+func logicalOperator() {
+	and := true && false
+	fmt.Printf("and: %v\n", and)
+	or := true || false
+	fmt.Printf("or: %v\n", or)
+	not := !false
+	fmt.Printf("not: %v\n", not)
+}
+
+func addressOperator() {
+	var x int = 1
+	fmt.Printf("x: %v\n", x)
+	var y *int = &x
+	fmt.Printf("y: %v\n", y)
+	fmt.Printf("*y: %v\n", *y)
+	z := *&*y + 3
+	fmt.Printf("z: %v\n", z)
+	fmt.Printf("y: %v\n", *y)
+	fmt.Printf("x: %v\n", x)
+
+	// Change data source pointer
+	var number = 5
+	fmt.Printf("number before: %v\n", number)
+	change := func(src *int, value int) {
+		*src = value
+	}
+	change(&number, 10)
+	fmt.Printf("number after: %v\n", number)
+}
+
+func conversion() {
+	type Person struct {
+		Name    string
+		Address *struct {
+			Street string
+			City   string
+		}
+	}
+
+	var data *struct {
+		Name    string `json:"name"`
+		Address *struct {
+			Street string `json:"street"`
+			City   string `json:"city"`
+		} `json:"address"`
+	}
+
+	var person = (*Person)(data) // ignoring tags, the underlying types are identical
+	fmt.Printf("person: %v\n", person)
+
+	hello := []byte("hellø")
+	fmt.Printf("hello: %v\n", hello)
+
+	a := 10
+	b := string(a)
+	fmt.Printf("b: %v\n", b)
+}
+
+func constExpression() {
+	const b = 15 / 4
+	fmt.Printf("b: %v\n", b)
+}
+
+func orderOfEvaluation() {
+	a := 1
+	f := func() int { a++; return a }
+	x := []int{a, f()} // x may be [1, 2] or [2, 2]: evaluation order between a and f() is not specified
+	fmt.Printf("x: %v\n", x)
+	m := map[int]int{a: 1, a: 2} // m may be {2: 1} or {2: 2}: evaluation order between the two map assignments is not specified
+	fmt.Printf("m: %v\n", m)
+	n := map[int]int{a: f()} // n may be {2: 3} or {3: 3}: evaluation order between the key and the value is not specified
+	fmt.Printf("n: %v\n", n)
+}
+
 func main() {
 	expSelector()
 	methodExpression()
@@ -162,4 +316,13 @@ func main() {
 	typeAssertion()
 	callFunc()
 	variadic()
+	instantiations()
+	typeUnification()
+	operators()
+	comparisonOperator()
+	logicalOperator()
+	addressOperator()
+	conversion()
+	constExpression()
+	orderOfEvaluation()
 }
